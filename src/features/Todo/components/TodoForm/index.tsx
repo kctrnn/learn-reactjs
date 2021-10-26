2eff5822c4277fb0b18b3cec1788d5fe5df9cee3
Kim Chan <chantran.24hdev@gmail.com>
2021-10-27 00:38:14 +0700

    Add yup validation

--- src/features/Todo/components/TodoForm/index.tsx
+++ src/features/Todo/components/TodoForm/index.tsx
@@ -1,15 +1,23 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { InputField } from 'components/FormFields';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
 export interface TodoFormValues {
   text: string;
 }
+
+const schema = yup.object().shape({
+  text: yup.string().required('Please enter todo').min(5, 'Todo is too short'),
+});
 
 function TodoForm() {
   const { control, handleSubmit } = useForm<TodoFormValues>({
     defaultValues: {
       text: '',
     },
+
+    resolver: yupResolver(schema),
   });
 
   const onSubmit = (formValues: TodoFormValues) => console.log(formValues);
