6a784a08e824a96cc3e438677a14a96f6c69326b
Kim Chan <chantran.24hdev@gmail.com>
2021-10-29 01:28:30 +0700

    Add LoginForm component

---
+++ src/features/Auth/components/LoginForm.tsx
@@ -0,0 +1,61 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button } from '@mui/material';
+import { InputField, PasswordField } from 'components/FormFields';
+import { LoginPayload } from 'models';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+export interface LoginFormProps {
+  onSubmit?: (formValues: LoginPayload) => void;
+}
+
+const schema = yup.object().shape({
+  username: yup
+    .string()
+    .required('Missing username')
+    .min(4, 'Username should have at least 4 characters'),
+  password: yup
+    .string()
+    .required('Missing password')
+    .min(6, 'Password should have at least 6 characters'),
+});
+
+function LoginForm({ onSubmit }: LoginFormProps) {
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<LoginPayload>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const handleFormSubmit = async (formValues: LoginPayload) => {
+    if (onSubmit) {
+      await onSubmit(formValues);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+      <InputField name="username" control={control} label="Username" />
+      <PasswordField name="password" control={control} label="Password" />
+
+      <Button
+        disabled={isSubmitting}
+        type="submit"
+        variant="contained"
+        color="primary"
+        fullWidth
+        size="large"
+      >
+        Sign in
+      </Button>
+    </form>
+  );
+}
+
+export default LoginForm;
